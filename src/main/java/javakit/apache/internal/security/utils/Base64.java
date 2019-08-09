d2eb211359d76818e36a20d4ff11289d09818da9
wechat <admin@eeob.com>
2019-08-09 10:39:29 +0800

    20190809

---
+++ src/main/java/javakit/apache/internal/security/utils/Base64.java
@@ -0,0 +1,6 @@
+package javakit.apache.internal.security.utils;
+
+public class Base64 {
+
+
+}
--- src/main/java/wangyonglin/utils/JavaKitBase64.java
+++ src/main/java/wangyonglin/utils/JavaKitBase64.java
@@ -1,13 +1,6 @@
 package wangyonglin.utils;
-
-
-
-
-
 import com.sun.org.apache.xml.internal.security.utils.Base64;
-
 import java.math.BigInteger;
-
 public class JavaKitBase64 {
     public static String hex(String hex) {
         BigInteger bi= new BigInteger(hex,16);
